c8e69477de1171abaf5c4e0115b522cc3b4067e4
Brandon Carrell <brandoncarrell@gmail.com>
2013-12-23 14:34:32 -0500

    [add] comments

--- verify.go
+++ verify.go
@@ -5,11 +5,14 @@ type verifier struct {
 	Results map[string]bool
 }
 
+// verification initializer
 func Verify(s string) *verifier {
 	v := &verifier{Query: s, Results: make(map[string]bool)}
 	return v
 }
 
+// a convenient finalizing function which confirms that all previously chained
+// methods passed their verifications
 func (v *verifier) IsVerified() bool {
 	for _, isValid := range v.Results {
 		if !isValid {
